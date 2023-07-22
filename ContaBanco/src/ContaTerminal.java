17ff4ec9a471f85621364d3aa5bebae2037b845e
vmarks765 <vinicius.p.marques@live.com>
2023-07-21 21:57:12 -0300

    projeto-final

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -10,13 +10,13 @@ public class ContaTerminal {
         System.out.print("Identifique-se! Digite seu nome:  ");
         String nome = sc.nextLine();
         System.out.printf("Seja bem-vindo %s, digite o número de sua Agência:  ", nome);
-        int agencia = sc.nextInt();
+        String agencia = sc.nextLine();
         System.out.print("Agora, digite o número de sua Conta:  ");
         int conta = sc.nextInt();
         System.out.print("Por fim, digite o seu saldo:  ");
         double saldo = sc.nextDouble();
 
-        String mensagem = String.format("\nOlá %s, obrigado por criar uma conta em nosso banco, sua agência é %d, " +
+        String mensagem = String.format("\nOlá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, " +
                 "conta %d e seu saldo de R$ %.2f já está disponível para saque.\n", nome, agencia, conta, saldo);
 
         System.out.println(mensagem);
